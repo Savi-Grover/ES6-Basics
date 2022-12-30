d16ea8249e4ba7d975764b8b0a73cd21d731000a
Savi Grover <savig447@gmail.com>
2022-12-30 02:35:09 -0500

    LET keyword

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 //------------------VAR keyword
-function sayHello(){
-  for ( var i =0; i<5; i++){
+// function sayHello(){
+//   for ( var i =0; i<5; i++){
+//     console.log(i);
+//   }
+//   console.log(i);
+// }
+// sayHello();
+// //O/p - 0,1,2,3,4,5
+
+//------------------LET KEYWORD
+function sayHello1(){
+  for ( let i =0; i<5; i++){
     console.log(i);
   }
-  console.log(i);
+ //console.log(i);  //error - i not defined
 }
-sayHello();
-//O/p - 0,1,2,3,4,5
+sayHello1();
+
 
 
 
